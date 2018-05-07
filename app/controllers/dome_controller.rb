3e2b4badce0d76581b0e0997467e214c38a5ea03
myvary <727484328@qq.com>
2018-05-07 15:26:05 +0800

    添加测试页面

---
+++ app/controllers/dome_controller.rb
@@ -0,0 +1,7 @@
+class DomeController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'dome/index'
+
+  get 'dome/show'
+  root 'dome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
