1a7397312305275f31da82c38c9c192c05bab60b
myvary <727484328@qq.com>
2018-05-08 16:37:29 +0800

    修改controller

--- app/controllers/dome_controller.rb
+++
@@ -1,7 +0,0 @@
-class DomeController < ApplicationController
-  def index
-  end
-
-  def show
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
-  get 'dome/index'
+  get 'article/index'
 
-  get 'dome/show'
-  root 'dome#index'
+  get 'article/show'
+
+  get 'article/new'
+
+  get 'article/edit'
+
+  root 'article#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
