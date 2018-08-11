82207fad95f0432909c8b920cba8f1e18368c833
myvary <727484328@qq.com>
2018-08-11 11:57:51 +0800

    添加链接

--- app/controllers/tag_controller.rb
+++ app/controllers/tag_controller.rb
@@ -1,6 +1,7 @@
 class TagController < ApplicationController
   def index
     @tag = params[:tag_name]
-    @articles = Tag.find_by(tag_name: params[:tag_name]).articles.page(params[:page]).per(15)
+    @articles = Tag.find_by(tag_name: params[:tag_name]).articles.order(id: :desc).page(params[:page]).per(15)
+    @tags = Tag.all.order(id: :desc)
   end
 end
