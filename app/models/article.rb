37f4f47373e8f113fd318c60f46d26af015c4e80
myvary <727484328@qq.com>
2018-12-28 13:00:31 +0800

    article_to_md

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,4 +4,27 @@ class Article < ApplicationRecord
   has_many :tags, through: :tag_article_relationships,dependent: :destroy
   extend FriendlyId
   friendly_id :title
+
+  class << self
+
+  	def article_to_md
+  		all = Article.all
+  		all.each do |a|
+  			file = File.open("/Users/liuxin/Me/blog/other/" + a.title+'.md','ab+')
+
+  			print a.title
+  			tags = ''
+  			a.tags.each do |t|
+  				tags+=t.tag_name.to_s+','
+  			end
+  			tags =  tags[0,tags.size-1]
+  			str = "```\r\ntags:" + tags.to_s + "\r\n```\r\n"
+  			body = str + a.content
+  			file.syswrite(body)
+  		end
+  	end
+
+
+  end
+
 end
