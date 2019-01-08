944e2a027f40e6a7e9c0bc68e58b17ddb57bdb40
myvary <727484328@qq.com>
2019-01-08 18:01:23 +0800

    修复文章数错误

--- app/controllers/tag_controller.rb
+++ app/controllers/tag_controller.rb
@@ -1,7 +1,8 @@
 class TagController < ApplicationController
   def index
     @tag = params[:tag_name]
-    @articles = Tag.find_by(tag_name: params[:tag_name]).articles.order(id: :desc).page(params[:page]).per(5)
-    @tags = Tag.all.order(id: :desc)
+    @articles_all = Tag.includes(:articles).includes(:tag_article_relationships).find_by(tag_name: params[:tag_name]).articles
+    @articles = @articles_all.order(id: :desc).page(params[:page]).per(5)
+    @tags = Tag.includes(:articles).includes(:tag_article_relationships).order(id: :desc)
   end
 end
