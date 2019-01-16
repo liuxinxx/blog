f9825f3b0a40c8101da7e50f3b5f06500d48a42c
myvary <727484328@qq.com>
2019-01-16 18:00:05 +0800

    添加缓存

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  config.cache_store = [:dalli_store, APP_CONFIG["memcached_host"], { namespace: "ns", compress: true }]
+  config.cache_store = [:dalli_store]
   # Code is not reloaded between requests.
   config.cache_classes = true
 
